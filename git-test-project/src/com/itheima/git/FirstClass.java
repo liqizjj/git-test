ca25cd139445cfe5696557fc64d8d31e25d63305
liqi <liqi@itcast.cn>
2022-05-18 14:59:45 +0800

    把本地工程添加到git 三次提交

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -4,13 +4,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git project");
         System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
-        System.out.println("hello git project");
+
 
 
     }
