4bb3079add7b19b8304e8e8a06c1ad14c5b083d9
liqi <liqi@itcast.cn>
2022-05-18 15:18:47 +0800

    修改FirstClass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -6,11 +6,11 @@ public class FirstClass {
         System.out.println("hello git project");
 
 
-    //Hello test
+    //Hello test aaaaaaaaaaaaaaaaaaa
     }
     public void set(){
         int o =1111111111;
-        int od =11113111111; int os =1111111111;int ddos =666666666;
+        int od =111; int os =1111111111;int ddos =666666666;
         int u=102;
         System.out.println(o);
     }
